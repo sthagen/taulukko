685716c894ef7b86606df7e3253491e7d5abdc32
Stefan Hagen <stefan@hagen.link>
2021-12-01 21:58:05 +0100

    Bend the CLI tests until these pass with the accidental guessplementation

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,10 +9,10 @@ import taulukko.cli as cli
 
 
 def test_main_legacy_ok(capsys):
-    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
-    assert cli.main(['translate', inp]) == 0
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', '2x2-table-w-th.html'))
+    assert cli.main(['extract', inp]) == 0
     out, err = capsys.readouterr()
-    assert 'translating html tree from' in out.lower()
+    assert 'extracting html tables from' in out.lower()
     assert not err
 
 
@@ -25,30 +25,20 @@ def test_version_ok(capsys):
         assert not err
 
 
-def test_translate_ok(capsys):
-    in_path = pathlib.Path('tests', 'fixtures', 'basic', 'export.zip')
+def test_extract_ok(capsys):
+    in_path = pathlib.Path('tests', 'fixtures', 'basic', '2x2-table-w-th.html')
     with pytest.raises(SystemExit) as exec_info:
-        cli.translate(source=in_path, inp=in_path)
+        cli.extract(source=in_path, inp=in_path)
         assert exec_info.value.code == 0
         out, err = capsys.readouterr()
-        assert 'would translate html tree' in out.lower()
+        assert 'would extract html tables' in out.lower()
         assert not err
 
 
-def test_translate_non_zip(capsys):
-    in_path = pathlib.Path('tests', 'fixtures', 'basic', 'no.zip')
+def test_translate_non_existing_html(capsys):
+    in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.html')
     with pytest.raises(SystemExit) as exec_info:
-        cli.translate(source=in_path, inp=in_path)
-        assert exec_info.value.code == 1
-        out, err = capsys.readouterr()
-        assert 'wrong magic number in zipfile' in out.lower()
-        assert not err
-
-
-def test_translate_non_existing_zip(capsys):
-    in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.zip')
-    with pytest.raises(SystemExit) as exec_info:
-        cli.translate(source=in_path, inp=in_path)
+        cli.extract(source=in_path, inp=in_path)
         assert exec_info.value.code == 1
         out, err = capsys.readouterr()
         assert 'source' in out.lower()

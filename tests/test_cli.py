b332041240c96125d4eb9bbea8033b54745759cf
Stefan Hagen <stefan@hagen.link>
2021-12-01 18:01:45 +0100

    Added copy-pasta tests guaranteed to fail to start fast (WIP)

---
+++ tests/test_cli.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import click
+import pytest
+
+import taulukko.cli as cli
+
+
+def test_main_legacy_ok(capsys):
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
+    assert cli.main(['translate', inp]) == 0
+    out, err = capsys.readouterr()
+    assert 'translating html tree from' in out.lower()
+    assert not err
+
+
+def test_version_ok(capsys):
+    with pytest.raises(click.exceptions.Exit) as exec_info:
+        cli.app_version() == 0
+        assert exec_info.value.code == 0
+        out, err = capsys.readouterr()
+        assert 'version' in out.lower()
+        assert not err
+
+
+def test_translate_ok(capsys):
+    in_path = pathlib.Path('tests', 'fixtures', 'basic', 'export.zip')
+    with pytest.raises(SystemExit) as exec_info:
+        cli.translate(source=in_path, inp=in_path)
+        assert exec_info.value.code == 0
+        out, err = capsys.readouterr()
+        assert 'would translate html tree' in out.lower()
+        assert not err
+
+
+def test_translate_non_zip(capsys):
+    in_path = pathlib.Path('tests', 'fixtures', 'basic', 'no.zip')
+    with pytest.raises(SystemExit) as exec_info:
+        cli.translate(source=in_path, inp=in_path)
+        assert exec_info.value.code == 1
+        out, err = capsys.readouterr()
+        assert 'wrong magic number in zipfile' in out.lower()
+        assert not err
+
+
+def test_translate_non_existing_zip(capsys):
+    in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.zip')
+    with pytest.raises(SystemExit) as exec_info:
+        cli.translate(source=in_path, inp=in_path)
+        assert exec_info.value.code == 1
+        out, err = capsys.readouterr()
+        assert 'source' in out.lower()
+        assert 'is no file' in out.lower()
+        assert not err
---
+++ tests/test_taulukko.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import taulukko.taulukko as ko
+
+
+def test_ko_main():
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
+    assert ko.main(['translate', inp]) == 0
+
+
+def test_ko_verify_request_too_few():
+    assert ko.verify_request([1]) == (2, 'received wrong number of arguments', [''])
+
+
+def test_ko_verify_request_unknown_command():
+    assert ko.verify_request(['unknown', 'does not matter']) == (2, 'received unknown command', [''])
+
+
+def test_ko_verify_request_falsy_input():
+    argv = ['translate', '']
+    assert ko.verify_request(argv) == (0, '', argv)
+
+
+def test_ko_verify_request_input_has_wrong_extension():
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'wrong.extension'))
+    assert ko.verify_request(['translate', inp]) == (1, 'source has not .zip extension', [''])

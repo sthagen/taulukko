c51a09e87b252a5decbff92eecc44eae30fa0eb0
Stefan Hagen <stefan@hagen.link>
2022-10-25 21:38:40 +0200

    FUture init for package and python 3.11.0 exemptions from defunct packages

--- taulukko/__init__.py
+++ taulukko/__init__.py
@@ -1,2 +1,69 @@
-"""Meeting, rendezvous, confluence (Finnish kohtaaminen) mark up, down, and up again."""
-__version__ = '2021.12.3'
+"""Table (Finnish Taulukko) glued together to transform into hands-free living."""
+import datetime as dti
+import logging
+import os
+import pathlib
+from typing import List, no_type_check
+
+# [[[fill git_describe()]]]
+__version__ = '2022.10.23+parent.d04133d9'
+# [[[end]]] (checksum: 18fdfd17580575137d602b77a9411178)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+
+APP_NAME = 'Table (Finnish Taulukko) glued together to transform into hands-free living.'
+APP_ALIAS = 'taulukko'
+APP_ENV = 'TAULUKKO'
+COMMA = ','
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = '.kiirastuli.json'
+DEFAULT_LF_ONLY = 'YES'
+log = logging.getLogger()  # Module level logger is sufficient
+LOG_FOLDER = pathlib.Path('logs')
+LOG_FILE = f'{APP_ALIAS}.log'
+LOG_PATH = pathlib.Path(LOG_FOLDER, LOG_FILE) if LOG_FOLDER.is_dir() else pathlib.Path(LOG_FILE)
+LOG_LEVEL = logging.INFO
+
+TS_FORMAT_LOG = '%Y-%m-%dT%H:%M:%S'
+TS_FORMAT_PAYLOADS = '%Y-%m-%d %H:%M:%S.%f UTC'
+
+__all__: List[str] = [
+    'APP_ALIAS',
+    'APP_NAME',
+    'COMMA',
+    'DEBUG',
+    'log',
+    'parse_csl',
+]
+
+
+@no_type_check
+def formatTime_RFC3339(self, record, datefmt=None):  # noqa
+    """HACK A DID ACK we could inject .astimezone() to localize ..."""
+    return dti.datetime.fromtimestamp(record.created, dti.timezone.utc).isoformat()  # pragma: no cover
+
+
+@no_type_check
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global log  # pylint: disable=global-statement
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': TS_FORMAT_LOG,
+        # 'filename': LOG_PATH,
+        'level': LOG_LEVEL if level is None else level,
+    }
+    logging.Formatter.formatTime = formatTime_RFC3339
+    logging.basicConfig(**log_format)
+    log = logging.getLogger(APP_ENV if name is None else name)
+    log.propagate = True
+
+
+init_logger(name=APP_ENV, level=logging.DEBUG if DEBUG else None)

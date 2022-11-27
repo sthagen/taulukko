7f8c9c59ade6ae63f39dab1355a98613bccc57eb
Stefan Hagen <stefan@hagen.link>
2022-11-27 19:10:57 +0100

    Migrated from click to fastapi test runner

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,46 +1,28 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
 import pathlib
 
-import click
-import pytest
+from typer.testing import CliRunner
 
-import taulukko.cli as cli
+import taulukko
+from taulukko.cli import app
 
-
-def test_main_legacy_ok(capsys):
-    inp = str(pathlib.Path('test', 'fixtures', 'basic', '2x2-table-w-th.html'))
-    assert cli.main(['extract', inp]) == 0
-    out, err = capsys.readouterr()
-    assert 'extracting html tables from' in out.lower()
-    assert not err
+runner = CliRunner()
 
 
-def test_version_ok(capsys):
-    with pytest.raises(click.exceptions.Exit) as exec_info:
-        cli.app_version() == 0
-        assert exec_info.value.code == 0
-        out, err = capsys.readouterr()
-        assert 'version' in out.lower()
-        assert not err
+def test_version_ok():
+    result = runner.invoke(app, ['version'])
+    assert result.exit_code == 0
+    assert f'version {taulukko.__version__}' in result.stdout
 
 
 def test_extract_ok(capsys):
     in_path = pathlib.Path('test', 'fixtures', 'basic', '2x2-table-w-th.html')
-    with pytest.raises(SystemExit) as exec_info:
-        cli.extract(source=in_path, inp=in_path)
-        assert exec_info.value.code == 0
-        out, err = capsys.readouterr()
-        assert 'would extract html tables' in out.lower()
-        assert not err
+    result = runner.invoke(app, ['extract', str(in_path), '--input', str(in_path)])
+    assert result.exit_code == 0
+    assert 'extracting html tables from' in result.stdout
 
 
-def test_translate_non_existing_html(capsys):
+def test_extract_non_existing_html(capsys):
     in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.html')
-    with pytest.raises(SystemExit) as exec_info:
-        cli.extract(source=in_path, inp=in_path)
-        assert exec_info.value.code == 1
-        out, err = capsys.readouterr()
-        assert 'source' in out.lower()
-        assert 'is no file' in out.lower()
-        assert not err
+    result = runner.invoke(app, ['extract', str(in_path), '--input', str(in_path)])
+    assert result.exit_code == 1
+    assert 'is no file' in result.stdout
